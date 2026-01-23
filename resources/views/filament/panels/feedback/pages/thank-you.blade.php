b2b1af62f13cb34b7b449ec025948b0cb4824f36
MrckJd <dmarckjude25@gmail.com>
2026-01-23 14:15:25 +0800

    feat: Enhance thank you page layout with additional feedback submission button

--- resources/views/filament/panels/feedback/pages/thank-you.blade.php
+++ resources/views/filament/panels/feedback/pages/thank-you.blade.php
@@ -2,12 +2,27 @@
     <div class="flex flex-col items-center justify-center min-h-[60vh] text-center space-y-6">
         <h1 class="text-3xl font-bold">Thank You!</h1>
         <p class="text-lg text-gray-600">We appreciate your feedback and will get back to you if necessary.</p>
+
+
+    <div class="space-y-2">
+
+        <x-filament::button
+            href="{{ url()->previous() }}"
+            color="gray"
+            tag="a"
+            class="w-full"
+        >
+            <span >Submit Another Feedback</span>
+        </x-filament::button>
+
         <x-filament::button
             href="/"
             color="primary"
             tag="a"
+            class="w-full"
         >
             Go to Homepage
         </x-filament::button>
+    </div>
     </div>
 </div>
